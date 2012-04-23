768fe1e7e7474f5277478095996d73c094eb6aa1
Arda C. Tugay <tugayac@rose-hulman.edu>
2012-04-23 03:06:54 -0400

    Updated Control Panel and created Player class

--- ElementalTowerDefense/src/PlayerTest.java
+++ ElementalTowerDefense/src/PlayerTest.java
@@ -11,17 +11,80 @@ import org.junit.Test;
  */
 public class PlayerTest {
 
+	private Player player;
+
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
+		this.player = new Player();
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() {
+		this.player = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testIsInstantiated() {
+		assertNotNull(this.player);
 	}
 
+	@Test
+	public void testCanSetAndGetScore() {
+		this.player.setScore(100);
+		assertEquals(100, this.player.getScore());
+
+		this.player.setScore(-100);
+		assertEquals(0, this.player.getScore());
+	}
+
+	@Test
+	public void testIncrementAndDecrementScore() {
+		this.player.setScore(50);
+		this.player.decScore(100);
+		assertEquals(0, this.player.getScore());
+
+		this.player.setScore(50);
+		this.player.incScore(150);
+		assertEquals(200, this.player.getScore());
+	}
+
+	@Test
+	public void testCanSetAndGetMana() {
+		this.player.setMana(1000);
+		assertEquals(1000, this.player.getMana());
+
+		this.player.setMana(-1000);
+		assertEquals(0, this.player.getMana());
+	}
+
+	@Test
+	public void testIncrementAndDecrementMana() {
+		this.player.setMana(1000);
+		this.player.decMana(1100);
+		assertEquals(0, this.player.getMana());
+
+		this.player.setMana(1000);
+		this.player.incMana(100);
+		assertEquals(1100, this.player.getMana());
+	}
+
+	@Test
+	public void testCanSetAndGetHealth() {
+		this.player.setHealth(200);
+		assertEquals(200, this.player.getHealth());
+
+		this.player.setHealth(-200);
+		assertEquals(0, this.player.getHealth());
+	}
+
+	@Test
+	public void testIncrementAndDecrementHealth() {
+		this.player.setHealth(200);
+		this.player.decHealth(300);
+		assertEquals(0, this.player.getHealth());
+
+		this.player.setHealth(200);
+		this.player.incHealth(100);
+		assertEquals(300, this.player.getHealth());
+	}
 }
